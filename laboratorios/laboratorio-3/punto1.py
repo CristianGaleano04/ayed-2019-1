d27373f80d207aa85b8e311bdb5e5660762f6901
unknown <2155421@labinfo.is.escuelaing.edu.co>
2019-01-31 08:37:10 -0500

    punto1

---
+++ laboratorios/laboratorio-3/punto1.py
@@ -0,0 +1,12 @@
+from sys import stdin
+from timeit import timeit
+
+def merge(numeros):
+    print(timeit)
+
+
+def main():
+  numeros = list(map(int,stdin.readline().strip().split(' ')))
+  merge(numeros)
+
+main()
